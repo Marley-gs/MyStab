f56526fe0ad4a4c47b97df4abf0f5ebff0611e73
Sara-ghobrial <58557538+Sara-ghobrial@users.noreply.github.com>
2020-02-22 20:57:11 +0100

    Add files via upload

---
+++ Code/MPU_TEST/MPU_TEST.ino
@@ -0,0 +1,54 @@
+#include "Wire.h"  // Arduino Wire library
+#include "I2Cdev.h"  //Installer ces 2 librairies
+#include "MPU6050.h"
+#include "math.h"
+#include<Servo.h>
+
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;  //mesures brutes
+int16_t gx, gy, gz;
+uint8_t Accel_range;
+uint8_t Gyro_range;
+float angle=0;
+
+Servo servoX;
+float val1;
+
+void setup() {
+  Wire.begin();  //I2C bus
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB (LEONARDO)
+  }
+
+  // initialize device
+  Serial.println("Initialisation I2C...");
+  accelgyro.initialize();
+
+  // verify connection
+  Serial.println("Test de la conection du dispositif ...");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection reussie" : "MPU6050 connection echec");
+  delay(1000);
+
+  servoX.attach(10);
+  servoX.write(90);
+}
+
+void loop() {
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  angle=0.98*(angle+float(gy)*0.01/131) + 0.02*atan2((double)ax,(double)az)*180/PI;
+  Serial.println(angle); 
+  delay(10);
+
+  //val1 = map (angle,0,180, 1000,2000);
+  //val2 = map (val_y,0,180, 1000,2000);
+  //Serial.println("valeur=");Serial.print(val1);
+  
+  //Serial.println(val2);
+  
+  servoX.write(angle);
+  //servoY.write(val2);
+}
